295779ba14ea7031124d6dd538c67067d3c4bb61
Kevin Audreylius <114163461+KevinAudreylius@users.noreply.github.com>
2024-08-28 13:02:41 +0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,28 +10,28 @@ const Projects = () => {
             <div data-aos="fade-up">
                 <a href="https://github.com/KevinAudreylius/Mid-Project-LnT" target="_blank" rel="noopener noreferrer">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
-                        <Image src="/images/project1.jpg" alt='portofolio' layout="fill" className='object-contain' />
+                        <Image src="/images/Project1.jpg" alt='portofolio' layout="fill" className='object-contain' />
                     </div>
                 </a>
             </div>
             <div data-aos="fade-up" data-aos-delay="300">
                 <a href="https://github.com/KevinAudreylius/Ngekos-Web-" target="_blank" rel="noopener noreferrer">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
-                        <Image src="/images/project2.png" alt='portofolio' layout="fill" className='object-contain' />
+                        <Image src="/images/Project2.png" alt='portofolio' layout="fill" className='object-contain' />
                     </div>
                 </a>
             </div>
             <div data-aos="fade-up" data-aos-delay="600">
                 <a href="https://github.com/KevinAudreylius/Jakarta-Convention-Center-JCC-" target="_blank" rel="noopener noreferrer">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
-                        <Image src="/images/project3.png" alt='portofolio' layout="fill" className='object-contain' />
+                        <Image src="/images/Project3.png" alt='portofolio' layout="fill" className='object-contain' />
                     </div>
                 </a>
             </div>
             <div data-aos="fade-up" data-aos-delay="900">
                 <a href="https://github.com/KevinAudreylius/Portofolio-2024" target="_blank" rel="noopener noreferrer">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
-                        <Image src="/images/project4.png" alt='portofolio' layout="fill" className='object-contain' />
+                        <Image src="/images/Project4.png" alt='portofolio' layout="fill" className='object-contain' />
                     </div>
                 </a>
             </div>
